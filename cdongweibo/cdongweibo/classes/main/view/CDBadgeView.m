72afd2a1dc489c93f271990f4b08dfd9548ae22b
陈栋 <chendong@yitaosoft.com>
2016-03-15 16:40:13 +0800

    BadgeView frame大小设置
    BadgeView frame大小设置

--- cdongweibo/cdongweibo/classes/main/view/CDBadgeView.m
+++ cdongweibo/cdongweibo/classes/main/view/CDBadgeView.m
@@ -7,14 +7,28 @@
 //
 
 #import "CDBadgeView.h"
-
+#import "NSString+utility.h"
+#import "CDUitiity.h"
 #define CDBadgeViewFont [UIFont systemFontOfSize:11]
 
 @implementation CDBadgeView
 
 
-
-
+//根据badgeValue值开控制按钮显示和隐藏
+- (void)setBadgeValue:(NSString *)badgeValue
+{
+    _badgeValue = badgeValue;
+//    self.titleLabel.text = _badgeValue;
+    [self setTitle:badgeValue forState:UIControlStateNormal];
+    self.hidden = [NSString isBlankString:_badgeValue];
+    
+    //计算文字图片大小
+    CGSize textSize = [badgeValue sizeWithFont:CDBadgeViewFont];
+//    CDLog(@"%f---%f",textSize.width,self.width);
+    if (textSize.width>self.width) {
+        self.width = textSize.width;
+    }
+}
 
 
 
@@ -30,7 +44,7 @@
         [self setBackgroundImage:[UIImage imageNamed:@"main_badge"] forState:UIControlStateNormal];
         //设置字体大小
         self.titleLabel.font = CDBadgeViewFont;
-        
+        self.titleLabel.textColor = [UIColor blackColor];
         [self sizeToFit];
     }
     return self;
