f7c0a5a25295ba0ec9aaa9ab78b300090e583b38
陈栋 <chendong@yitaosoft.com>
2016-03-15 16:10:28 +0800

    TabBarButton计算 子控件的frame
    TabBarButton计算 子控件的frame

--- cdongweibo/cdongweibo/classes/main/view/CDTabBarButton.m
+++ cdongweibo/cdongweibo/classes/main/view/CDTabBarButton.m
@@ -8,7 +8,9 @@
 
 #import "CDTabBarButton.h"
 #import "CDBadgeView.h"
+#import "CDUitiity.h"
 
+#define CDBarImageRadio 0.7
 @interface CDTabBarButton ()
 
 @property (nonatomic,weak) CDBadgeView *badgeView;
@@ -33,8 +35,8 @@
 - (void)setItem:(UITabBarItem *)item
 {
     _item = item;
-    //设置按钮nomar图片 和selectEd 下图
-//    [self addObserver:self forKeyPath:nil options:nil context:nil];
+    //这里需要手动调用一次observeValueForKeyPath方法为button赋值
+    [self observeValueForKeyPath:nil ofObject:nil change:nil context:nil];
     
     //KVO:时刻监听一个对象有没有变化
     [item addObserver:self forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:nil];
@@ -53,8 +55,33 @@
 }
 
 
+//修改按钮内部子控件的frame
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    CGSize btnSize = self.bounds.size;
+    
 
 
+    //image
+    CGFloat imageX = 0;
+    CGFloat imageY = 0;
+    CGFloat imageW = btnSize.width;
+    CGFloat imageH = btnSize.height * CDBarImageRadio;
+    self.imageView.frame = CGRectMake(imageX, imageY, imageW, imageH);
+    
+    //title
+    CGFloat titleW = btnSize.width;
+    CGFloat titleH = 10;
+    CGFloat titleX = 0;
+    CGFloat titley = imageH;
+    self.titleLabel.frame = CGRectMake(titleX, titley, titleW, titleH);
+    self.titleLabel.font = [UIFont systemFontOfSize:11];
+    
+    //badgeView
+    self.badgeView.x = self.width/2+5;
+    self.badgeView.y = 0;
+}
 
 
 //按钮初始化时候的设置
@@ -65,9 +92,10 @@
         //设置字体颜色
         [self setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [self setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
-        //图片居中
-        
-        //文字居中
+        // 图片居中
+        self.imageView.contentMode = UIViewContentModeCenter;
+        // 文字居中
+        self.titleLabel.textAlignment = NSTextAlignmentCenter;
         //文字字体
     }
     return self;
