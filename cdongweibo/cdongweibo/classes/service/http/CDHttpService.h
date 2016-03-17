d27f0ad046dc46ccead34db2db1f67dccf5d1fbd
陈栋 <chendong@yitaosoft.com>
2016-03-18 00:17:37 +0800

    get请求封装 减少对AFNetworking 的依赖
    get请求封装 减少对AFNetworking 的依赖

---
+++ cdongweibo/cdongweibo/classes/service/http/CDHttpService.h
@@ -0,0 +1,27 @@
+//
+//  CDHttpService.h
+//  cdongweibo
+//
+//定制 http请求协议
+//  Created by 陈栋 on 16/3/17.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+@interface CDHttpService : NSObject
+
+
+/**
+ *  get请求 减少对AFNetworking 的依赖
+ *
+ *  @param URLString  请求头
+ *  @param parameters 请求参数
+ *  @param success    请求成功处理block
+ *  @param failure    请求失败处理block
+ */
++ (void)GET:(NSString *)URLString parameters:(id)parameters
+    success:(void (^)(id responseObject))success
+    failure:(void (^)(NSError *error))failure;
+
+@end
---
+++ cdongweibo/cdongweibo/classes/service/http/CDHttpService.m
@@ -0,0 +1,40 @@
+//
+//  CDHttpService.m
+//  cdongweibo
+//
+//  Created by 陈栋 on 16/3/17.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "CDHttpService.h"
+
+
+@implementation CDHttpService
+
+
+/**
+ *  get请求 减少对AFNetworking 的依赖
+ *
+ *  @param URLString  请求头
+ *  @param parameters 请求参数
+ *  @param success    请求成功处理block
+ *  @param failure    请求失败处理block
+ */
++ (void)GET:(NSString *)URLString parameters:(id)parameters
+    success:(void (^)(id responseObject))success
+    failure:(void (^)(NSError *error))failure
+{
+    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
+    
+    [mgr GET:URLString parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if (success) {
+            success(responseObject);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        failure(error);
+    }];
+    
+    
+}
+
+@end
