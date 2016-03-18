859938e061f858cc54360220ee6f739cd015afa5
陈栋 <chendong@yitaosoft.com>
2016-03-18 08:20:35 +0800

    抽取微博数据获取 统一参数管理接口
    抽取微博数据获取 统一参数管理接口

---
+++ cdongweibo/cdongweibo/classes/service/weibo/CDWeiBoSystemParams.h
@@ -0,0 +1,21 @@
+//
+//  CDWeiBoSystemParams.h
+//  cdongweibo
+//
+//  Created by 陈栋 on 16/3/18.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define CD_ACCESS_TOKEN_URL @"https://api.weibo.com/oauth2/access_token"
+#define CD_AUTHORIZE_URL @"https://api.weibo.com/oauth2/authorize"
+#define CD_CLIENT_ID @"2355429710"
+#define CD_REDIRECT_URL @"http://www.baidu.com"
+#define CD_CLIENT_SECRET @"4d9e6693f9fa7cfa3b44a197d9698fd1"
+#define CD_GRANT_TYPE @"authorization_code"
+
+
+@interface CDWeiBoSystemParams : NSObject
+
+@end
---
+++ cdongweibo/cdongweibo/classes/service/weibo/CDWeiBoSystemParams.m
@@ -0,0 +1,13 @@
+//
+//  CDWeiBoSystemParams.m
+//  cdongweibo
+//
+//  Created by 陈栋 on 16/3/18.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "CDWeiBoSystemParams.h"
+
+@implementation CDWeiBoSystemParams
+
+@end
