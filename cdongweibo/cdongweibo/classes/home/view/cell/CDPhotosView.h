8897616620b79e052a6676fb70a1b361a4cb76ef
陈栋 <chendong@yitaosoft.com>
2016-03-21 17:13:38 +0800

    自定义视图三步走：第一步自定义视图

---
+++ cdongweibo/cdongweibo/classes/home/view/cell/CDPhotosView.h
@@ -0,0 +1,18 @@
+//
+//  CDPhotosView.h
+//  cdongweibo
+//
+//  Created by 陈栋 on 16/3/21.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CDStatusFrame.h"
+
+@interface CDPhotosView : UIView
+
+//视图模型
+@property (nonatomic,strong) CDStatusFrame *statusFrame;
+
+
+@end
